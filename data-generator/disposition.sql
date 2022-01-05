d438b8613230b790d1bcf2969a16a898a488ee88
FORGE-JITA\ktokt <ulfnew@gmail.com>
2022-01-05 14:45:20 +0500

    Update supporting sql scripts

--- data-generator/disposition.sql
+++ data-generator/disposition.sql
@@ -1,73 +1,16 @@
 /*
  * storage-for-all-things
- * Copyright © 2021 Volkhin Nikolay
- * 20.11.2021, 13:49
+ * Copyright © 2022 Volkhin Nikolay
+ * 05.01.2022, 14:45
  */
 
-/* poor */
-with const as (select 5 as POOR, 40 as RICH)
-select code
-from const c,
-     essence e
-         join essence_attribute ea on e.id = ea.essence_id
-group by code, c.POOR, c.RICH
-having count(*) >= c.POOR
-   and count(*) <= (c.POOR * 2 - 1)
-;
-/* average */
-with const as (select 5 as POOR, 40 as RICH)
-select code
-from const c,
-     essence e
-         join essence_attribute ea on e.id = ea.essence_id
-group by code, c.POOR, c.RICH
-having count(*) >= c.POOR * 2
-   and count(*) <= (c.RICH / 2)
-;
-/* rich */
-with const as (select 5 as POOR, 40 as RICH)
-select code
-from const c,
-     essence e
-         join essence_attribute ea on e.id = ea.essence_id
-group by code, c.POOR, c.RICH
-having count(*) >= (c.RICH / 2)
-   and count(*) <= c.RICH
-;
-
-/* few */
-with const as (select 3 as FEW, 24 as MANY)
-select code
-from const c,
-     essence e
-         join essence_thing ea on e.id = ea.essence_id
-group by code, c.FEW, c.MANY
-having count(*) >= c.FEW
-   and count(*) <= (c.FEW * 2 - 1)
-;
-/* average */
-with const as (select 3 as FEW, 24 as MANY)
-select code
-from const c,
-     essence e
-         join essence_thing ea on e.id = ea.essence_id
-group by code, c.FEW, c.MANY
-having count(*) >= c.FEW * 2
-   and count(*) <= (c.MANY / 2)
-;
-/* many */
-with const as (select 3 as FEW, 24 as MANY)
-select code
-from const c,
-     essence e
-         join essence_thing ea on e.id = ea.essence_id
-group by code, c.FEW, c.MANY
-having count(*) >= (c.MANY / 2 + 1)
-   and count(*) <= c.MANY
-;
+/*
+how many things: FEW | AVERAGE | MANY
+how many attributes: POOR | AVERAGE | RICH
+*/
 
 /* FEW + POOR */
-with const as (select 3 as FEW, 24 as MANY, 5 as POOR, 40 as RICH),
+with const as (select 3 as FEW, 300 as MANY, 5 as POOR, 140 as RICH),
      few as (select code, count(*) as items
              from const c,
                   essence e
@@ -85,10 +28,10 @@ from few as t
          join poor as p on t.code = p.code
 order by t.items, p.params
 ;
-/* shirt */
+/* salad */
 
 /* AVERAGE + AVERAGE*/
-with const as (select 3 as FEW, 24 as MANY, 5 as POOR, 40 as RICH),
+with const as (select 3 as FEW, 300 as MANY, 5 as POOR, 140 as RICH),
      items as (select code, count(*) as items
                from const c,
                     essence e
@@ -108,10 +51,10 @@ from items as t
          join params as p on t.code = p.code
 order by t.items, p.params
 ;
-/* elephant */
+/* sugar */
 
 /* MANY and RICH */
-with const as (select 3 as FEW, 24 as MANY, 5 as POOR, 40 as RICH),
+with const as (select 3 as FEW, 300 as MANY, 5 as POOR, 140 as RICH),
      many as (select code, count(*) as items
               from const c,
                    essence e
@@ -129,4 +72,66 @@ from many as t
          join rich as p on t.code = p.code
 order by t.items, p.params
 ;
-/* roof */
+/* underclothes */
+
+/* poor */
+with const as (select 5 as POOR, 140 as RICH)
+select code
+from const c,
+     essence e
+         join essence_attribute ea on e.id = ea.essence_id
+group by code, c.POOR, c.RICH
+having count(*) >= c.POOR
+   and count(*) <= (c.POOR * 2 - 1)
+;
+/* average */
+with const as (select 5 as POOR, 140 as RICH)
+select code
+from const c,
+     essence e
+         join essence_attribute ea on e.id = ea.essence_id
+group by code, c.POOR, c.RICH
+having count(*) >= c.POOR * 2
+   and count(*) <= (c.RICH / 2)
+;
+/* rich */
+with const as (select 5 as POOR, 140 as RICH)
+select code
+from const c,
+     essence e
+         join essence_attribute ea on e.id = ea.essence_id
+group by code, c.POOR, c.RICH
+having count(*) >= (c.RICH / 2)
+   and count(*) <= c.RICH
+;
+
+/* few */
+with const as (select 3 as FEW, 300 as MANY)
+select code
+from const c,
+     essence e
+         join essence_thing ea on e.id = ea.essence_id
+group by code, c.FEW, c.MANY
+having count(*) >= c.FEW
+   and count(*) <= (c.FEW * 2 - 1)
+;
+/* average */
+with const as (select 3 as FEW, 300 as MANY)
+select code
+from const c,
+     essence e
+         join essence_thing ea on e.id = ea.essence_id
+group by code, c.FEW, c.MANY
+having count(*) >= c.FEW * 2
+   and count(*) <= (c.MANY / 2)
+;
+/* many */
+with const as (select 3 as FEW, 300 as MANY)
+select code
+from const c,
+     essence e
+         join essence_thing ea on e.id = ea.essence_id
+group by code, c.FEW, c.MANY
+having count(*) >= (c.MANY / 2 + 1)
+   and count(*) <= c.MANY
+;
